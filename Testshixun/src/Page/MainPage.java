d5378162e24d2f83190dfa6f40f17a6a62edfc89
sunjinbiao <254272669@qq.com>
2020-12-06 09:32:26 +0800

    1

--- Testshixun/src/Page/MainPage.java
+++ Testshixun/src/Page/MainPage.java
@@ -45,7 +45,7 @@ public class MainPage extends BaseTest{
 	public WebElement exit_urldescribe() {
 		return driver.findElement(By.id("endUrlDescription"));	
 	}
-	//±£´æ
+	//±£´æ°´¼ü
 	public WebElement save_form() {
 		return  driver.findElement(By.id("save-form-button"));	
 	}
