71f69f76ff2fd733a067160479b53cfe03f50b5c
sunjinbiao <254272669@qq.com>
2020-12-07 07:59:55 +0800

    1

---
+++ Testshixun/src/com/test/demo/SendReport.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+import java.io.UnsupportedEncodingException;
+
+import com.test.utils.MailUtil;
+
+public class SendReport {
+	
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		MailUtil.sendEmail();
+	}
+	
+}
